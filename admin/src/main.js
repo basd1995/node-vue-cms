7ac47d49a958580f011fea73276eac8c9600c9e0
barry <barry.cao@bizfocus.cn>
2019-07-30 10:29:11 +0800

    增删改查接口 改为通用CRUD接口

--- admin/src/main.js
+++ admin/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import http from './http'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = http;
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -3,13 +3,38 @@ module.exports = app => {
 
   const router = express.Router();
 
-  const categroy = require('../../models/Category');
-
-  router.post('/categories', async (req, res) => {
-    const model = await categroy.create(req.body);
-    console.log(req.body);
+  //save
+  router.post('/', async (req, res) => {
+    const model = await req.model.create(req.body);
     res.send(model);
   });
 
-  app.use('/admin/api', router);
+  //update
+  router.put('/:id', async (req, res) => {
+    const model = await req.model.findByIdAndUpdate(req.params.id, req.body);
+    res.send(model);
+  });
+
+  //delete
+  router.delete('/:id', async (req, res) => {
+    const model = await req.model.findByIdAndDelete(req.params.id);
+    res.send(model);
+  });
+
+  //get list
+  router.get('/', async (req, res) => {
+    const items = await req.model.find().limit(10)
+    res.send(items)
+  });
+
+  //get item by id
+  router.get('/:id', async (req, res) => {
+    const model = await req.model.findById(req.params.id);
+    res.send(model)
+  });
+
+  app.use('/admin/api/rest/:resource', async (req, res) => {
+    const modelName = require('inflection').classify(req.params.resource);
+    req.model = require(`../../models/${modelName}`)
+  }, router);
 }
